15ece8a956d3efdb9837ecfd17ab58cf84cbab30
Nick Stewart <nicholas4@gmail.com>
2012-07-11 16:51:09 -0600

    stuff

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,4 +1,10 @@
 class Tree
+  
+  def initialize(root_node)
+    @root_node = root_node
+    @node_hash = root_node.node_hash
+  end
+  
   def to_s
     return self.object_id
   end
--- lib/tree_factory.rb
+++ lib/tree_factory.rb
@@ -1,5 +1,8 @@
 class TreeFactory
   def self.build
-    return Tree.new
+    # Build Nodes
+    
+    root_node = NodeFactory.build
+    return Tree.new(root_node)
   end
 end
